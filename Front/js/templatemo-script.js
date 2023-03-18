6da42cfb2815ed50e11e60341a9e7aba59e9b411
ava-dana <avadanaei@gmail.com>
2023-03-19 01:29:43 +0330

    Final color palette. Added "read more" button.

--- Front/js/templatemo-script.js
+++ Front/js/templatemo-script.js
@@ -16,3 +16,19 @@ $(function() {
         $(".tm-header").removeClass("show");
       });
 });
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "بیشتر..";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "کمتر..";
+    moreText.style.display = "inline";
+  }
+};
