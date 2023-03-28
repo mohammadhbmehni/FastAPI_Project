2deeb8e337412fc8ccc38cef3c48fb02e0d35e2d
Houtan TV <houtantv@gmail.com>
2023-03-28 18:28:31 +0430

    backend is connected to front

--- backend.py
+++ backend.py
@@ -1,15 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 import models as db
-
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+
+templates = Jinja2Templates(directory=r"C:\Users\User\Desktop\pycharm maktab\Front")
+
 
 db.Base.metadata.create_all(db.engine)
 
 app = FastAPI()
-
+app.mount("/Front/css", StaticFiles(directory="Front"), name="Front")
 
 @app.get("/")
-def home():
+def home(request : Request):
     our_posts = db.session.query(db.Post).all()
     post = {
         "post_id" : "",
@@ -32,4 +37,45 @@ def home():
         post["time"] = i.time
         resault["posts"].append(post)
     resault["number_of_posts"] = len(our_posts)
-    return resault
+    return templates.TemplateResponse("index.html", {"request":request,"resault":resault})
+
+@app.get("/post/{post_id}")
+def show_post(request : Request, post_id):
+    our_post = db.session.query(db.Post).filter(db.Post.post_id == post_id).one()
+    our_comments = db.session.query(db.Comments).filter(db.Comments.post_id == our_post.post_id)
+    comment_schema = {
+        "comment_id" : "",
+        "user_id" : "",
+        "post_id" : "",
+        "text" : "",
+        "time" : ""
+    }
+    resault = {
+        "post": {
+            "post_id": our_post.post_id,
+            "author": our_post.poster_id,
+            "title": our_post.title,
+            "text": our_post.text,
+            "summary": our_post.summary,
+            "time": our_post.time
+        },
+        "comments" : []
+    }
+    for i in our_comments:
+        comment_schema["comment_id"] = i.comment_id
+        comment_schema["user_id"] = i.user_id
+        comment_schema["post_id"] = i.post_id
+        comment_schema["text"] = i.text
+        comment_schema["time"] = i.time
+        resault["comments"].append(comment_schema)
+
+    return templates.TemplateResponse("post.html", {"request":request,"resault":resault})
+
+@app.get("/about")
+def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request":request})
+
+
+@app.get("/contact")
+def contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request":request})
